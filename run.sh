a66436d6c65659005d63628c2ec67e64625723d4
StudentWeis <studentweis@gmail.com>
2023-07-20 16:13:34 +0800

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
-cd ~/mpi/v3
+cd ~/MPI_Neuron
+
+# 虚拟环境
+conda activate mpineuron
+pip install numpy matplotlib psutil
 
 # LIF
-export DISPLAY=:0
 gcc -fPIC -shared LIF/LIF.c -o LIF/LIF.so -O3
-mpiexec --allow-run-as-root -n 6 python LIF/LIF.py
+mpiexec -n 4 python LIF/LIF_Nums.py
 
 # HH
 export DISPLAY=:0
