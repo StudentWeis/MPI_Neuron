db4a5e0233dc1f6eae83ba212155f58b5da0ac33
StudentWeis <studentweis@gmail.com>
2023-08-29 17:17:53 +0800

    Izhikevich C 库搞定

--- Izhikevich/Izhikevich.c
+++ Izhikevich/Izhikevich.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 void izhikevich(float Vmi, float ui, float Iji, float a, float b, float *vt, float *ut)
 {
     vt[0] = 0.04 * Vmi * Vmi + 5 * Vmi + 140 - ui + Iji;
@@ -11,17 +9,15 @@ void rungeKutta(float *Vm, float *u, float *Ij, float a, float b, float c, float
     for (int i = 0; i < numNeu; i++)
     {
         izhikevich(Vm[i], u[i], Ij[i], a, b, &v1, &u1);
-        izhikevich(Vm[i] + 0.5 * v1, *u + 0, Ij[i], a, b, &v2, &u2);
-        izhikevich(Vm[i] + 0.5 * v2, *u + 0.5 * u2, Ij[i], a, b, &v3, &u3);
-        izhikevich(Vm[i] + v3, *u + u3, Ij[i], a, b, &v4, &u4);
-
-        printf("first: v1=%f, v2=%f, v3=%f, v4=%f\n, ", v1, v2, v3, v4);
-        Vm[i] += (1 / 6) * v1 + (1 / 3) * v2 + (1 / 3) * v3 + (1 / 6) * v4;
-        u[i] += (1 / 6) * u1 + (1 / 3) * u2 + (1 / 3) * u3 + (1 / 6) * u4;
-        // if (Vm[i] >= 30)
-        // {
-        //     Vm[i] = c;
-        //     u[i] = u[i] + d;
-        // }
+        izhikevich(Vm[i] + 0.05 * v1, u[i] + 0.05 * u1, Ij[i], a, b, &v2, &u2);
+        izhikevich(Vm[i] + 0.05 * v2, u[i] + 0.05 * u2, Ij[i], a, b, &v3, &u3);
+        izhikevich(Vm[i] + 0.1 * v3, u[i] + 0.1 * u3, Ij[i], a, b, &v4, &u4);
+        Vm[i] += ((0.1 * v1 / 6) + (0.1 * v2 / 3) + (0.1 * v3 / 3) + (0.1 * v4 / 6));
+        u[i] += ((0.1 * u1 / 6) + (0.1 * u2 / 3) + (0.1 * u3 / 3) + (0.1 * u4 / 6));
+        if (Vm[i] >= 30)
+        {
+            Vm[i] = c;
+            u[i] += d;
+        }
     }
 }
--- Izhikevich/Izhikevich.py
+++ Izhikevich/Izhikevich.py
@@ -1,13 +1,9 @@
-from ctypes import c_float, c_int
 import os
+from ctypes import c_float, c_int
 
 import numpy as np
 import numpy.ctypeslib as ctl
 from mpi4py import MPI
-import matplotlib
-import matplotlib.pyplot as plt
-
-matplotlib.use('Agg')
 
 # 初始化 MPI 配置
 comm = MPI.COMM_WORLD
@@ -26,17 +22,54 @@ ctl_lib.rungeKutta.argtypes = [
     c_float, c_float, c_float, c_float, c_int
 ]
 
-def main(niter, numNeuron):
-    a = 0.05  # 恢复变量的时间尺度，越小，恢复越慢
+def process_Neuron(niter: int, numNeuron: int):
+    # a = 0.05  # 恢复变量的时间尺度，越小，恢复越慢
+    # b = 0.2  # 恢复变量依赖膜电位的阈值下随机波动的敏感度
+    # c = -50.0  # 膜电位复位值
+    # d = 5  # 恢复变量复位值
+
+    a = 0.02  # 恢复变量的时间尺度，越小，恢复越慢
     b = 0.2  # 恢复变量依赖膜电位的阈值下随机波动的敏感度
     c = -50.0  # 膜电位复位值
-    d = 5  # 恢复变量复位值
-    Vm = np.ones(numNeuron, dtype=np.single) * (-65)
+    d = 2  # 恢复变量复位值
+    Vm = np.ones(numNeuron, dtype=np.single) * (-55)
     u = np.ones(numNeuron, dtype=np.single) * (-0)
     Ij = np.ones(numNeuron, dtype=np.single) * (5)
-    for i in range(niter):  # 模拟时长(ms)
 
+    # 主进程
+    if comm_rank == 0:
+        import time
+
+        import matplotlib
+        import matplotlib.pyplot as plt
+        matplotlib.use('Agg')
+        numPlot = 10000
+        # 打印发送辅助信息
+        print("本程序为 Izhikevich 神经元的 MPI 分布式仿真")
+        print("Linux MPI 版本为")
+        print("使用 C 动态库进行加速计算")
+        print("使用 Matplotlib 绘图")
+        print("神经元数量为：", numNeuron)
+        print("迭代次数为：", niter)
+        # 绘图数组
+        picU = np.ones(numPlot, dtype=np.single)
+
+    for i in range(niter):
         ctl_lib.rungeKutta(Vm, u, Ij, a, b, c, d, numNeuron)
+        # 记录单个神经元的膜电位数据
+        if comm_rank == 0:
+            if (i < numPlot):
+                picU[i] = Vm[5]
+    
+        # 主进程发送辅助信息
+    if comm_rank == 0:
+        # 绘制单个神经元的膜电位曲线验证仿真正确性
+        x = np.linspace(0, numPlot, numPlot)
+        figU, ax = plt.subplots()
+        ax.plot(x, picU)
+        figU.savefig("Izhikevich 膜电位.png")
 
-
-main(1000, 10)
+if __name__ == '__main__':
+    # 初始化仿真参数
+    niters = 10000  # 迭代次数
+    process_Neuron(niters, 10)
